190f4a6a9195e7dce935b7658d9b6d11985dfbe9
leshkovichpvl <leshkovichpvl@gmail.com>
2018-04-17 05:34:38 +0300

    Revert: small fix

--- dist/LoadableDataRecord.d.ts
+++ dist/LoadableDataRecord.d.ts
@@ -1,22 +1,22 @@
-import { List } from 'immutable';
-declare const LoadableDataPackRecord_base: any;
+import { List, Record } from 'immutable';
+declare const LoadableDataPackRecord_base: Record.Class;
 export declare class LoadableDataPackRecord<T> extends LoadableDataPackRecord_base {
     readonly startIndex: number;
     readonly stopIndex: number;
     readonly items: List<T>;
-    static createInstance<T>(startIndex: number, stopIndex: number, items: List<T>): any;
+    static createInstance<T>(startIndex: number, stopIndex: number, items: List<T>): LoadableDataPackRecord<T>;
     hasIndex(index: number): boolean;
     getItem(index: number): T;
     incrementIndexes(count?: number): LoadableDataPackRecord<T>;
     set<K extends string, V>(k: K, v: V): LoadableDataPackRecord<T>;
     private getRange();
 }
-declare const LoadableDataRecord_base: any;
+declare const LoadableDataRecord_base: Record.Class;
 export declare class LoadableDataRecord<T> extends LoadableDataRecord_base {
     readonly dataCount: number;
     readonly packs: List<LoadableDataPackRecord<T>>;
-    static createInstance<T>(dataCount: number, packs: List<LoadableDataPackRecord<T>>): any;
-    static createNullInstance<T>(): any;
+    static createInstance<T>(dataCount: number, packs: List<LoadableDataPackRecord<T>>): LoadableDataRecord<T>;
+    static createNullInstance<T>(): LoadableDataRecord<T>;
     addPack(pack: LoadableDataPackRecord<T>): LoadableDataRecord<T>;
     setDataCount(count: number): LoadableDataRecord<T>;
     unShiftList(list: List<T>): LoadableDataRecord<T>;
